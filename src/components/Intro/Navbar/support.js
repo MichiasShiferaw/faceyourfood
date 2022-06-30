e45dda109902b82588d770afcc09797beaa0c0e7
Michias Shiferaw <mshif073@uottawa.ca>
2022-06-29 20:57:37 -0400

    support button

--- src/components/Intro/Navbar/support.js
+++ src/components/Intro/Navbar/support.js
@@ -2,10 +2,8 @@ import React from 'react'
 
 function support() {
   return (
-    <div className='text-end'>
-        {/* <button type="button" className="col align-self-end w-25 text-right m-3 p-2 fixed-bottom bottom-0 end-0 btn btn-success">Support?</button> */}
-    {/* <div className="fixed-bottom text-end border w-25">Support?</div> */}
-     <a  className=" text-end float-right fixed-bottom w-25 btn btn-primary btn-lg m-3 p-2"  type="submit" role="button">Support?</a> </div>
+    // fixed-bottom w-25 btn btn-success btn-lg
+     <a  className="supporting btn btn-success btn-lg m-3 p-2 rounded-pill"  type="submit" role="button">Support?</a>
   )
 }
 
